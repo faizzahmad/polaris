caf50cc9b784e1b7cee01ddd4efbb4b22479d41f
faiz ahmad <faizahmedkgn85@gmail.com>
2026-03-12 21:02:05 +0530

    created project view page and also added logic for create project

---
+++ src/features/projects/hooks/use-projects.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+export const useProjects = () => {
+ return useQuery(api.project.get);
+};
+
+export const useProjectsPartials = (limit : number) => {
+ return useQuery(api.project.getPartial,{limit});
+};
+
+export const useCreateprojects = () => {
+    return useMutation(api.project.create).withOptimisticUpdate(
+        (localStore,args) => {
+            const existingProjects = localStore.getQuery(api.project.get);
+            if(existingProjects !== undefined) {
+                const now = Date.now();
+                const newProject = {
+                    _id : crypto.randomUUID() as Id<"projects">,
+                    _creationTime : now,
+                    name : args.name,
+                    ownerId : "anonymus",
+                    updatedAt : now,
+                };
+                localStore.setQuery(api.project.get,{},[
+                    newProject,
+                    ...existingProjects,
+                ]);
+            }
+        }
+    )
+};
