1d449c2337759c4ba6c07978bdb9029060f1baaf
faiz ahmad <faizahmedkgn85@gmail.com>
2026-03-13 22:07:18 +0530

    added create page and create folder flow

---
+++ src/features/projects/components/file-explorer/tree.tsx
@@ -0,0 +1,49 @@
+import { ChevronRightIcon } from "lucide-react";
+import { FileIcon, FolderIcon } from "@react-symbols/icons/utils";
+import { cn } from "@/lib/utils";
+import {
+    useCreateFile, useCreateFolder,
+    useFolderContents, useRenameFile, useDeleteFile
+} from "../../hooks/use-file";
+import { getItemPadding } from "./constants";
+import { LoadingRow } from "./loading-row";
+import { CreateInput } from "./create-input";
+import { Doc, Id } from "../../../../../convex/_generated/dataModel";
+import { useState } from "react";
+
+export const Tree = ({
+    item,
+    level = 0,
+    projectId,
+}: {
+    item: Doc<"files">;
+    level?: number;
+    projectId: Id<"projects">;
+}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isRenaming, setIsRenaming] = useState(false);
+    const [creating, setCreating] = useState<"file" | "folder" | null>(null);
+
+    const renameFile = useRenameFile();
+    const delteFile = useDeleteFile();
+    const createFile = useCreateFile();
+    const createFolder = useCreateFolder();
+    const folderContents = useFolderContents({
+        projectId,
+        parentId: item._id,
+        enabled: item.type === "folder" && isOpen,
+    });
+
+    if(item.type === "file"){
+        return(
+            <div>
+                I am a file!
+            </div>
+        )
+    }
+    return(
+        <div>
+            I am a folder!
+        </div>
+    )
+}
